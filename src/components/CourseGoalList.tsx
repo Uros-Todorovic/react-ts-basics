51579adf5449727e55c7d5392b0ce399328c6a72
Uroš Todorović <todorovic.urosh@gmail.com>
2023-11-15 09:26:27 +0100

    finished

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,6 +1,7 @@
-import { type FC } from 'react';
+import { type FC, type ReactNode } from 'react';
 import { CourseGoal as CGType } from '../App.tsx';
 import CourseGoal from './CourseGoal.tsx';
+import InfoBox from './InfoBox.tsx';
 
 interface CourseGoalProps {
 	goals: Array<CGType>;
@@ -8,8 +9,21 @@ interface CourseGoalProps {
 }
 
 const CourseGoalList: FC<CourseGoalProps> = ({ goals, onDeleteGoal }) => {
+	if (goals.length === 0) {
+		return <InfoBox mode="hint">You have no course goals yet. Start adding some!</InfoBox>;
+	}
+
+	let warningBox: ReactNode;
+	if (goals.length >= 4) {
+		warningBox = (
+			<InfoBox mode="warning" severity="high">
+				You are collecting too many goals. Don't put too much on your plate.
+			</InfoBox>
+		);
+	}
 	return (
 		<>
+			{warningBox}
 			<ul>
 				{goals.map((goal) => (
 					<li key={goal.id}>
